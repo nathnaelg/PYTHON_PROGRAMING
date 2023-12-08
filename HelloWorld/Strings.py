d6170fbda9bda15c82aaa66b30b6c9ac261dba62
nathnaelg <ngirma700@gmail.com>
2023-12-08 21:54:13 +0300

    Add Formatted strings

--- HelloWorld/Strings.py
+++ HelloWorld/Strings.py
@@ -1,4 +1,25 @@
 # Double quotes and Single quotes
 
-course =  'Python course for "All"'
+course =  "Python course for All"
 print (course)
+
+course =  'Python course for All'
+print (course)
+
+# ... Multiple line string...
+
+# samll email to john 
+
+email = '''
+hi John,
+
+Here is our first email to you.
+
+Thank you ,
+Nat
+
+'''
+
+print (email)
+
+# ...Formatted Strings.....
